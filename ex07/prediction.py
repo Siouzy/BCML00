7db6116ba453de6ec7f43589aa736dee5cb7c89b
Suzanne <suzanne.repellin@gmail.com>
2020-07-01 10:15:50 +0200

    ex07 thta reshape

--- ex07/prediction.py
+++ ex07/prediction.py
@@ -2,6 +2,7 @@ import numpy as np
 from tools import add_intercept
 
 def predict_(x, theta):
+    theta = theta.reshape((1, -1))
     x = add_intercept(x)
     y = x * theta
     return y.sum(axis=1)
